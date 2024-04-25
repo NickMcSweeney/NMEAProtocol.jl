86fce74428c68a0ba9d2a5c1ea80018873c2ffa9
NickMcSweeney <nick@shindler.tech>
2024-04-25 22:19:16 +0200

    try and fix tests

--- test/benchmark.test.jl
+++ test/benchmark.test.jl
@@ -14,7 +14,7 @@ export_markdown(results_path, results)
 judgement = judge(results, baseline)
 export_markdown(judgement_path, judgement, export_invariants=true)
 
-if(ENV["GITHUB_ACTIONS"] || ENV["GITHUB_ACTIONS"] === "true")
+if(ENV["GITHUB_ACTIONS"] === "true")
     @info "In CI: saving new baseline"
     PkgBenchmark.writeresults(baseline_path, results)
 else
